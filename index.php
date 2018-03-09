c0cbd074495d1d7b00fbe87dc51d322017d6003a
lgarciacarmona <36212408+lgarciacarmona@users.noreply.github.com>
2018-03-08 18:39:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,7 @@
 </head>
 <body>
 
-<?php 
-$numero1 = 2;
-$numero2 = 2;
 
-echo $numero1 * $numero2;
-
-$array = array ("burbuja","gorda");
-echo $array[0];
-?>
 
 <form  action="formulario.php" method="GET">
 	<input type="text" required="numero1" placeholder="numero" name="numero1"><br>
